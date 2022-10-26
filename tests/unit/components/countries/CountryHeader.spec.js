f50d030dd291b6c98209835ebb2febb10181efd8
Diogo Gutierre <diogorg@gmail.com>
2022-10-26 20:46:20 -0300

    test: add jest and basic suit

---
+++ tests/unit/components/countries/CountryHeader.spec.js
@@ -0,0 +1,27 @@
+import CountryHeader from '@/components/countries/CountryHeader.vue'
+import { shallowMount } from '@vue/test-utils'
+
+const global = {
+        components: {
+          'q-toolbar': 'q-toolbar',
+          'q-avatar': 'q-avatar',
+          'font-awesome-icon': 'font-awesome-icon',
+          'q-toolbar-title': 'q-toolbar-title'
+        }
+      }
+describe('CountryHeader.vue', () => {
+  const wrapper = shallowMount(CountryHeader, {global})
+
+  it('Should have tool bar with infos', () => {
+    expect(wrapper.html()).toContain('<q-toolbar class="bg-black text-white shadow-1 q-mb-sm">')
+  })
+  it('Should have my avatar', () => {
+    expect(wrapper.html()).toContain('<q-avatar><img src="https://avatars.githubusercontent.com/u/1072595?v=4"></q-avatar>')
+  })
+  it('Should have my github link', () => {
+    expect(wrapper.html()).toContain('<a target="_blank" href="https://github.com/diogorg">')
+  })
+  it('Should have my name', () => {
+    expect(wrapper.html()).toContain('Diogo Gutierre')
+  })
+})
